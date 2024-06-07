cede2c5dab1e1cedfce4352c22caa1950bef0047
SOUHAILMK99 <souhailmjirid935@gmail.com>
2024-06-07 15:58:47 +0000

    application web

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved BY SUPMTI</b>
+      </div>
+      <strong>Copyright &copy; 2024 <a href="">SOUHAIL</a></strong>
+    </div>
+</footer>
